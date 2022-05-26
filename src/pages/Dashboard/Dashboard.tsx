7d4fc27801712b68ae622b48a7c089af48ecb78a
erickbauer <hullbauer@gmail.com>
2022-05-26 15:22:05 -0300

    utilizando useState e useCallback em um context

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -6,18 +6,20 @@ import { useUsuarioLogado } from "../../shared/hooks";
 export const Dashboard = () => {
     const counterRef = useRef(0);
 
-    const usuarioLogadoContext = useUsuarioLogado();
+    const { nomeUsuario, logout} = useUsuarioLogado();
 
     return(
         <div>
             <p>Dashboard</p>
 
-            <p>{usuarioLogadoContext.nomeUsuario}</p>
+            <p>{nomeUsuario}</p>
 
             <p>Contador: {counterRef.current}</p>
 
             <button onClick={() => counterRef.current++}>Somar</button>
             <button onClick={() => console.log(counterRef.current)}>Log</button>
+
+            <button onClick={logout}>Logout</button>
 
             <Link to="/login">Login</Link>
         </div>
--- src/shared/contexts/UsuarioLogado.tsx
+++ src/shared/contexts/UsuarioLogado.tsx
@@ -1,7 +1,9 @@
-import { createContext } from "react"
+import { createContext, useCallback, useEffect, useState } from "react"
 
+// interface com os dados que o context vai prover
 interface IUsuarioLogadoContextData{
     nomeUsuario: string;
+    logout: () => void;
 }
 
 interface IChildren{
@@ -11,8 +13,21 @@ interface IChildren{
 export const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData>({} as IUsuarioLogadoContextData);
 
 export const UsuarioLogadoProvider: React.FC<IChildren> = ({children}) => {
+    const [nome, setNome] = useState("");
+
+    useEffect(() => {
+        setTimeout(() => {
+            setNome("Lucas")
+        }, 1000);
+    });
+
+    const handleLogout = useCallback(() => {
+        console.log("Logout executou")
+    }, [])
+
     return(
-        <UsuarioLogadoContext.Provider value={{nomeUsuario: "Lucas"}} >
+        // O context disponibiliza os dados para as tags que for children dele
+        <UsuarioLogadoContext.Provider value={{nomeUsuario: nome, logout: handleLogout}} >
             {children}
         </UsuarioLogadoContext.Provider>
     );
